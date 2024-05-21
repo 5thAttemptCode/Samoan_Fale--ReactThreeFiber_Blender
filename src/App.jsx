d433e59db210eff24455b15328367cb92c2edd5d
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-21 15:44:59 +0200

    global styles and LanguageProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import MainExperience from './canvasComponents/mainExperience'
 import Nav from './components/nav'
 import { Controls } from './components/controls'
 import { ToggledGroupsProvider } from './context/groupContext'
+import { LanguageProvider } from './context/languageContext'
 
 
 export default function App() {
 
   return (
-    <ToggledGroupsProvider>
-      <Nav />
-      <MainExperience />
-      <Controls />
-    </ToggledGroupsProvider>
+    <LanguageProvider>
+      <ToggledGroupsProvider>
+        <Nav />
+        <MainExperience />
+        <Controls />
+      </ToggledGroupsProvider>
+    </LanguageProvider>
   )
 }
